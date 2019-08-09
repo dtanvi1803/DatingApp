dd56788d0bcf3f0a6e57a714bd24a88a17e69846
Tanvi Desai <dtanvi1803@yahoo.co.in>
2019-08-09 10:28:14 +0530

    3rdPartyAlerifyBootswatch

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -10,15 +11,15 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private alertifyService: AlertifyService) { }
 
   ngOnInit() {
   }
   register() {
     this.authService.reigster(this.model).subscribe(() => {
-      console.log('registration successfull');
+      this.alertifyService.success('registration successfull');
     }, error => {
-      console.log(error);
+      this.alertifyService.error(error);
     });
   }
   cancel() {
